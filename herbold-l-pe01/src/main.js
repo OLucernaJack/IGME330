7c37e81f99c4a4d3bfc41327de6f9a0d396fe1b8
Lucy Herbold <xth4714@g.rit.edu>
2024-01-25 10:44:53 -0500

    hwfix and in class exercise

--- herbold-l-pe01/src/main.js
+++ herbold-l-pe01/src/main.js
@@ -1,3 +1,5 @@
+import { getRandom } from "./utils.js";
+
 const words1 = ["Acute", "Aft", "Anti-matter", "Bipolar", "Cargo", "Command", "Communication", "Computer", "Deuterium", "Dorsal", "Emergency", "Engineering", "Environmental", "Flight", "Fore", "Guidance", "Heat", "Impulse", "Increased", "Inertial", "Infinite", "Ionizing", "Isolinear", "Lateral", "Linear", "Matter", "Medical", "Navigational", "Optical", "Optimal", "Optional", "Personal", "Personnel", "Phased", "Reduced", "Science", "Ship's", "Shuttlecraft", "Structural", "Subspace", "Transporter", "Ventral"];
 
 const words2 = ["Propulsion", "Dissipation", "Sensor", "Improbability", "Buffer", "Graviton", "Replicator", "Matter", "Anti-matter", "Organic", "Power", "Silicon", "Holographic", "Transient", "Integrity", "Plasma", "Fusion", "Control", "Access", "Auto", "Destruct", "Isolinear", "Transwarp", "Energy", "Medical", "Environmental", "Coil", "Impulse", "Warp", "Phaser", "Operating", "Photon", "Deflector", "Integrity", "Control", "Bridge", "Dampening", "Display", "Beam", "Quantum", "Baseline", "Input"];
@@ -5,8 +7,6 @@ const words2 = ["Propulsion", "Dissipation", "Sensor", "Improbability", "Buffer"
 const words3 = ["Chamber", "Interface", "Coil", "Polymer", "Biosphere", "Platform", "Thruster", "Deflector", "Replicator", "Tricorder", "Operation", "Array", "Matrix", "Grid", "Sensor", "Mode", "Panel", "Storage", "Conduit", "Pod", "Hatch", "Regulator", "Display", "Inverter", "Spectrum", "Generator", "Cloud", "Field", "Terminal", "Module", "Procedure", "System", "Diagnostic", "Device", "Beam", "Probe", "Bank", "Tie-In", "Facility", "Bay", "Indicator", "Cell"];
 
 console.log(words1[0]);
-
-import { getRandom } from "./utils";
 
 const getWords = numLines => {
     for (let i = 0; i < numLines; i++) {
@@ -20,12 +20,7 @@ const getWords = numLines => {
         else {
             document.querySelector("#output").innerHTML += `<br>${firstWord} ${secondWord} ${thirdWord}`;
         }
-
     }
-
-
-
-
 }
 
 const init = () => {
@@ -36,4 +31,4 @@ const init = () => {
     getWords(1);
 }
 
-window.onload = init;
+init();
